7ac73b931803d6e628c988f0c1a19754a21788b6
yuanhuan <yuanhuan@tcl.com>
2014-06-20 17:57:52 +0800

    增加login代码
    Signed-off-by: yuanhuan <yuanhuan@tcl.com>

--- src/main/java/com/rideo/app/oauth2/controller/UserController.java
+++ src/main/java/com/rideo/app/oauth2/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.rideo.app.oauth2.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import net.sf.json.JSONObject;
 
 import org.springframework.security.core.Authentication;
@@ -10,6 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rideo.app.oauth2.domain.AppUserDetails;
@@ -21,18 +19,36 @@ public class UserController
 
     @RequestMapping(value = "info", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
-    String getUserInfo(HttpServletRequest request, HttpServletResponse response)
+    String getUserInfo()
     {
-        String contentType = "text/json;charset=UTF-8";
-        response.setContentType(contentType);
-        response.setCharacterEncoding("utf-8");
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication.getPrincipal() instanceof AppUserDetails)
         {
             AppUserDetails appUserDetails = (AppUserDetails) authentication.getPrincipal();
-            // System.out.println("appUserDetails=" + appUserDetails);
-            return JSONObject.fromObject(appUserDetails.getUser()).discard("password").toString();
+            return JSONObject.fromObject(appUserDetails.getUser()).discard("username")
+                    .discard("password").toString();
+        }
+
+        return "{}";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody
+    String login(@RequestParam(required = false) String phoneNumber,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = true) String password,
+            @RequestParam(required = true) String userType)
+    {
+        System.out.println("phoneNumber=" + phoneNumber);
+        System.out.println("email=" + email);
+        System.out.println("password=" + password);
+        System.out.println("userType=" + userType);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof AppUserDetails)
+        {
+            AppUserDetails appUserDetails = (AppUserDetails) authentication.getPrincipal();
+            return JSONObject.fromObject(appUserDetails.getUser()).discard("username")
+                    .discard("password").toString();
         }
 
         return "{}";
